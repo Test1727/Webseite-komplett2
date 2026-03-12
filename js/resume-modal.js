d83b500da203373b547c91154e9fc4f857a68a31
Test1727 <astridkraft.business@gmail.com>
2026-03-12 16:20:37 +0100

    Update resume-modal.js

--- js/resume-modal.js
+++ js/resume-modal.js
@@ -19,8 +19,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // PDF.js Worker konfigurieren
     if (isIOS || isSafari) {
-        pdfjsLib.GlobalWorkerOptions.workerSrc = 'js/lib/pdf.worker.js';
+        pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
     }
+
     
     // Übersetzungsfunktion für das Modal
     function updateModalTranslations(lang) {
